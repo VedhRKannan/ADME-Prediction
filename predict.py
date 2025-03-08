63506abc4fa54551f78baa69853b29ceb07e4a10
VedhRKannan <vedhkannan@gmail.com>
2025-03-08 14:30:22 +0000

    multiple models processing

--- predict.py
+++ predict.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 
 
 # Load model
-model = joblib.load("saved_models/SVR.pkl")
+# model = joblib.load("saved_models/SVR.pkl")
 
 # Make predictions
 def mol_to_fp(smiles, radius=2, nBits=1024):
@@ -21,6 +21,16 @@ def mol_to_fp(smiles, radius=2, nBits=1024):
     arr = np.zeros((nBits,), dtype=int)
     DataStructs.ConvertToNumpyArray(fp, arr)
     return arr
-predictions = model.predict([mol_to_fp("CC(=O)Nc1ccc(cc1)O")])
 
-print(predictions)
+
+
+# Load all models
+names = ["lip", "sol"]
+loaded_models = {name: joblib.load(f"saved_models/{name}.pkl") for name in names}
+
+smiles = "CC(=O)Nc1ccc(cc1)O"
+smiles = [mol_to_fp(smiles)]
+# Make predictions with all models
+for name, model in loaded_models.items():
+    preds = model.predict(smiles)
+    print(f"Predictions from {name}: {preds[:5]}")  # Print first 5 predictions
